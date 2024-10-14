aa96819ad6e162d2f12f787d849facf17d8fc57f
Kerpmisster <nta20030123@gmail.com>
2024-10-14 09:51:31 +0700

    Lesson01

---
+++ Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va ten: Nguyen Tuan Anh");
+            Console.WriteLine("Ngay sinh: 12/05/1997");
+            Console.WriteLine("Dia chi: Thieu Lam Tu");
+            Console.WriteLine("Dien Thoai: 098696969");
+            Console.WriteLine("Rat kich thich khi duoc hoc Csharp");
+            Console.Read();
+        }
+    }
+}
